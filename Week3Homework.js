46bdf79d524e737ea0ddfe91ccb04cf45638a570
Eric Baker <eric100596@aol.com>
2021-11-18 17:30:54 -0600

    Week 3 Homework

--- Week3Homework.js
+++ Week3Homework.js
@@ -1,7 +1,7 @@
-const pizzaToppings = ["sausage", "pepperoni", "cheese", "olives"];
+const toppings = ["sausage", "pepperoni", "cheese", "olives"];
 
 function greetCustomer () {
-  for (let topping of pizzaToppings) {
+  for (let topping of toppings) {
     console.log(topping);
   };
 
@@ -9,12 +9,28 @@ function greetCustomer () {
   greetCustomer(console.log("Welcome to E's Pizza! We have the following toppings: "));
 
 function getPizzaOrder (size, crust,...toppings) {
-  const myOrder = window.prompt("Pizza size: Enter x-large, large, or medium ");
-  const myOrder2 = window.prompt("Type of crust: Enter Pan, Hand Tossed, or Thin ");
+  const myOrder = window.prompt("Pizza size: Enter x-large, large, or medium");
+  const myOrder2 = window.prompt("Type of crust: Enter pan, hand tossed, or thin ");
   const myOrder3 = window.prompt("Toppings?: Enter sausage, pepperoni, cheese, and/or olives");
   console.log(`Here's your order: A ${myOrder} ${myOrder2} pizza, with ${myOrder3}`);
 }
 
-getPizzaOrder();
+getPizzaOrder(toppings);
+
+const sizes = ["extra large", "large", "medium"];
+const crusts = ["pan", "hand tossed", "thin"];
+
+function preparePizza(size, crust, topping) {
+
+  const myPizza = {
+size: "sizes",
+crust: "crusts",
+toppings:["sausage", "pepperoni", "cheese", "olives"],
+}
+
+}
+
+
+preparePizza(myPizza);
 
 
