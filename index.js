cb49bf3239eacb5bba61ee9a32ae1eef75f14f4b
Eric Baker <eric100596@aol.com>
2021-12-09 17:55:38 -0800

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+import { Header, Nav, Main, Footer } from "./components";
+import * as state from "./store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+
+const router = new Navigo(window.location.origin);
+
+router
+  .on({
+    "/": () => render(state.Home),
+    ":page": params => {
+      let page = capitalize(params.page);
+      render(state[page]);
+    }
+  })
+  .resolve();
+
+function render(st) {
+  document.querySelector("#root").innerHTML = `
+    ${Header(st)}
+    ${Nav(state.Links)}
+    ${Main(st)}
+    ${Footer()}
+  `;
+  router.updatePageLinks();
+  addEventListeners();
+}
+
+function addEventListeners() {
+  // add menu toggle to bars icon in nav bar
+  document
+    .querySelector(".fa-bars")
+    .addEventListener("click", () =>
+      document.querySelector("nav > ul").classList.toggle("hidden--mobile")
+    );
+
+  // populating gallery with pictures
+  const gallerySection = document.querySelector("#gallery");
+  // using modules to populate gallery with pictures
+  AddPicturesToGallery(GalleryPictures, gallerySection);
+
+  // handle form submission with PrintFormOnSubmit module
+  const form = document.querySelector("form");
+  PrintFormOnSubmit(form);
+}
